31f85727e683789cb36426c31c23326826348237
YSongdong <Yang1991>
2021-02-08 14:31:39 +0800

    计算出距离

--- DrawRect/ViewController.m
+++ DrawRect/ViewController.m
@@ -23,9 +23,10 @@
 @property (strong, nonatomic) GCDAsyncSocket *clientSocket;
 // 发送数据源
 @property (nonatomic,strong) DataByteModel *sendModel;
-
-@property (nonatomic,assign) CGPoint twoPoint;
-
+// 杆塔的中心点坐标
+@property (nonatomic,assign) CGPoint toweCenterPoint;
+// 说明lab
+@property (nonatomic,strong) UILabel *markLab;
 @end
 
 @implementation ViewController
@@ -34,37 +35,41 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     self.wRatio = 0;
-    
+    // 创建UI
+    [self createUI];
+}
+
+-(void) createUI{
     // 背景图
     self.bgimg = [UIImage imageNamed:@"WechatIMG2.jpeg"];
-    self.wRatio = self.bgimg.size.width/[UIScreen mainScreen].bounds.size.width;
+    self.wRatio = self.bgimg.size.width/KScreenW;
     double  h = self.bgimg.size.height/self.wRatio;
-    self.bgimgView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 80, [UIScreen mainScreen].bounds.size.width, h)];
+    self.bgimgView = [[UIImageView alloc]initWithFrame:CGRectMake(0, KSNaviTopHeight, KScreenW, h)];
     [self.view addSubview:self.bgimgView];
     self.bgimgView.image = self.bgimg;
     
     //手势视图
-    self.drawView = [[SDDrawBardView alloc]initWithFrame:CGRectMake(0, 80, [UIScreen mainScreen].bounds.size.width, h)];
+    self.drawView = [[SDDrawBardView alloc]initWithFrame:CGRectMake(0, KSNaviTopHeight, KScreenW, h)];
     [self.view addSubview:self.drawView];
     self.drawView.backgroundColor = [UIColor clearColor];
     self.drawView.state = noState;
     self.drawView.delegate = self;
     
     // 截图一
-    UIImageView *drawImagV = [[UIImageView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(self.drawView.frame)+10, 150, 200)];
+    UIImageView *drawImagV = [[UIImageView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(self.drawView.frame)+10, 120, 150)];
     [self.view addSubview:drawImagV];
     drawImagV.tag = 1000;
     
-    UIImageView *drawImagV1 = [[UIImageView alloc]initWithFrame:CGRectMake(180, CGRectGetMaxY(self.drawView.frame)+10, 150, 200)];
+    UIImageView *drawImagV1 = [[UIImageView alloc]initWithFrame:CGRectMake(160, CGRectGetMaxY(self.drawView.frame)+10, 120, 150)];
     [self.view addSubview:drawImagV1];
     drawImagV1.tag = 1001;
     
     NSArray *arr = @[@"截图一",@"截图二",@"确定"];
-    CGFloat w = (Width-40)/3;
+    CGFloat w = (KScreenW-40)/3;
     for (int i=0; i<arr.count; i++) {
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
         [self.view addSubview:btn];
-        btn.frame = CGRectMake(10+i*w+i*10, Height-80, w, 40);
+        btn.frame = CGRectMake(10+i*w+i*10, KScreenH-40-KSTabBarHeight, w, 40);
         [btn setTitle:arr[i] forState:UIControlStateNormal];
         [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [btn setTitleColor:[UIColor redColor] forState:UIControlStateSelected];
@@ -74,6 +79,12 @@
         btn.layer.borderColor = [UIColor blackColor].CGColor;
         [btn addTarget:self action:@selector(selectABtnAction:) forControlEvents:UIControlEventTouchUpInside];
     }
+    // 说明lab
+    self.markLab = [[UILabel alloc]initWithFrame:CGRectMake(20, KScreenH-KSTabBarHeight-70, KScreenW, 20)];
+    [self.view addSubview:self.markLab];
+    self.markLab.text = @"距离:  米";
+    self.markLab.textColor = [UIColor blackColor];
+    self.markLab.font = [UIFont systemFontOfSize:14];
 }
 -(void)selectABtnAction:(UIButton*)sender{
     switch (sender.tag-2000) {
@@ -81,26 +92,32 @@
         {
             UIButton *btn = [self.view viewWithTag:2001];
             btn.selected = NO;
+            btn.layer.borderColor = [UIColor blackColor].CGColor;
             self.drawView.state = oneState;
             sender.selected = YES;
+            sender.layer.borderColor = [UIColor redColor].CGColor;
             break;
         }
             
        case 1:
            {
-             UIButton *btn = [self.view viewWithTag:2000];
-              btn.selected = NO;
-             sender.selected = YES;
-             self.drawView.state = twoState;
+               UIButton *btn = [self.view viewWithTag:2000];
+               btn.selected = NO;
+               btn.layer.borderColor = [UIColor blackColor].CGColor;
+               sender.selected = YES;
+               sender.layer.borderColor = [UIColor redColor].CGColor;
+               self.drawView.state = twoState;
              break;
             }
             case 2:
             {
-//              UIButton *btn = [self.view viewWithTag:2000];
-//               btn.selected = NO;
-//              sender.selected = YES;
-              self.drawView.state = noState;
-                
+                UIButton *btn = [self.view viewWithTag:2000];
+                btn.selected = NO;
+                btn.layer.borderColor = [UIColor blackColor].CGColor;
+                UIButton *btn1 = [self.view viewWithTag:2001];
+                btn1.selected = NO;
+                btn1.layer.borderColor = [UIColor blackColor].CGColor;
+                self.drawView.state = noState;
                 [self createSocket];
               break;
              }
@@ -108,8 +125,6 @@
             break;
     }
 }
-
-
 
 -(void) touchesEndedStartPoint:(CGPoint)startPoint andEndPoint:(CGPoint)endPoint{
     CGFloat x = startPoint.x * self.wRatio;
@@ -127,7 +142,7 @@
     
     if (self.drawView.state ==  twoState) {
         // 中心点 xy
-        self.twoPoint= CGPointMake((startPoint.x * self.wRatio + endPoint.x * self.wRatio)/2 ,(startPoint.y * self.wRatio + endPoint.y * self.wRatio)/2);
+        self.toweCenterPoint= CGPointMake((startPoint.x * self.wRatio + endPoint.x * self.wRatio)/2 ,(startPoint.y * self.wRatio + endPoint.y * self.wRatio)/2);
         return;
     }
     self.sendModel = [[DataByteModel alloc]init];
@@ -157,7 +172,6 @@
     UIImage *img = [UIImage imageWithCGImage:imgRef];
     return img;
 }
-
 #pragma mark ------- socket-------
 -(void)createSocket{
     self.clientSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
@@ -209,7 +223,6 @@
     
     [byteData appendData:imageByteData];
     
-    
     NSMutableData *data = [[NSMutableData alloc]init];
     NSData *headerData = [self.sendModel.header dataUsingEncoding:NSUTF8StringEncoding];
     [data appendData:headerData];
@@ -220,9 +233,6 @@
     [data appendData:byteData];
     
     [self.clientSocket writeData:data withTimeout:-1 tag:0];
-    
-    NSLog(@"----%@---",[imageByteData hexString]);
-   
 }
 
 /**
@@ -233,15 +243,14 @@
  */
 - (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag{
     int index = 0;
-    
     //报头
-    NSData *headerData =[data subdataWithRange:NSMakeRange(0, 4)];
-    NSString *headerStr = [[NSString alloc]initWithData:headerData encoding:NSUTF8StringEncoding];
+//    NSData *headerData =[data subdataWithRange:NSMakeRange(0, 4)];
+//    NSString *headerStr = [[NSString alloc]initWithData:headerData encoding:NSUTF8StringEncoding];
     index += 4;
     
     // 剩余数据长度
-    NSData *data1 =[data subdataWithRange:NSMakeRange(index, 4)];
-    int totalLenght = *(int*)([data1 bytes]);
+//    NSData *data1 =[data subdataWithRange:NSMakeRange(index, 4)];
+//    int totalLenght = *(int*)([data1 bytes]);
     index += 4;
     
     //GUID 长度
@@ -250,13 +259,13 @@
     index += 4;
     
     //GUID
-    NSData *data3 =[data subdataWithRange:NSMakeRange(index, index+guidLenght)];
-    NSString *guidStr = [[NSString alloc]initWithData:data3 encoding:NSUTF8StringEncoding];
+//    NSData *data3 =[data subdataWithRange:NSMakeRange(index, index+guidLenght)];
+//    NSString *guidStr = [[NSString alloc]initWithData:data3 encoding:NSUTF8StringEncoding];
     index += guidLenght;
     
     //ParamertId
-    NSData *data4 =[data subdataWithRange:NSMakeRange(index, 4)];
-    int paramerId = *(int*)([data4 bytes]);
+//    NSData *data4 =[data subdataWithRange:NSMakeRange(index, 4)];
+//    int paramerId = *(int*)([data4 bytes]);
     index += 4;
     
     //重心点X
@@ -265,29 +274,36 @@
     index += 4;
     
     //重心点y
-    NSData *data6 =[data subdataWithRange:NSMakeRange(index, 4)];
-    int y = *(int*)([data6 bytes]);
+//    NSData *data6 =[data subdataWithRange:NSMakeRange(index, 4)];
+//    int y = *(int*)([data6 bytes]);
     index += 4;
     
     //距离
     NSData *data7 =[data subdataWithRange:NSMakeRange(index, 4)];
     int camDist = *(int*)([data7 bytes]);
-   
     
+    NSInteger rsult = [self getWithCalculatCeJuX:x andCamDist:camDist];
+    self.markLab.text = [NSString stringWithFormat:@"距离:  %ld 米",(long)rsult];
+}
+
+// 计算距离
+-(NSInteger) getWithCalculatCeJuX:(double)x andCamDist:(double)camDist{
+    NSInteger  distance = 0 ;
     // 图片总宽度
     double imageWidthRatio = self.bgimg.size.width / 108;
     // 物体的角度
     double objectAngle = x / imageWidthRatio;
     // 杆塔角度
-    double towerAngle = self.twoPoint.x / imageWidthRatio;
+    double towerAngle = self.toweCenterPoint.x / imageWidthRatio;
     // 夹角  取绝对值
     double includAngle = fabs(towerAngle - objectAngle);
+    //
+    double angle = 90 - includAngle;
+    // 距离
+    distance = (NSInteger) (double)camDist * fabs(angle/180*3.1415);
     
-    
-    NSLog(@"---camDist--%d----",camDist);
+    return  distance;
 }
-
-
 
 
 @end
